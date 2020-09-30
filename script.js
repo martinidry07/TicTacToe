6bd43a1aa373acbd814c815d1c559ca2852ed706
martinidry07 <70594234+martinidry07@users.noreply.github.com>
2020-09-30 21:00:23 +0300

    Pabeigts

--- script.js
+++ script.js
@@ -1,127 +1,37 @@
 let turn="X";
 console.log(turn);
+let win=false;
 let pmesage=document.getElementById('mesage');
 
 let reset_button=document.getElementById('reset');
 reset_button.addEventListener("click", function (){
-    b11_button.textContent = "";
-    b12_button.textContent = "";
-    b13_button.textContent = "";
-    b21_button.textContent = "";
-    b22_button.textContent = "";
-    b23_button.textContent = "";
-    b31_button.textContent = "";
-    b32_button.textContent = "";
-    b33_button.textContent = "";
+    turn="X";
+    for(thisButton of myButtons){
+        thisButton.style.background="khaki";
+        thisButton.textContent = "";
+    }
+    win=false;
+    pmesage.textContent="";
 });
 
-let b11_button=document.getElementById('b11');
-b11_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b11_button.textContent == ""){
-        b11_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
+let myButtons=document.getElementsByClassName("Button");
 
-let b12_button=document.getElementById('b12');
-b12_button.addEventListener("click", function (){
+for(thisButton of myButtons){
     pmesage.textContent="";
-    if(b12_button.textContent == ""){
-        b12_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
-
-let b13_button=document.getElementById('b13');
-b13_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b13_button.textContent == ""){
-        b13_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
-
-let b21_button=document.getElementById('b21');
-b21_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b21_button.textContent == ""){
-        b21_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
-
-let b22_button=document.getElementById('b22');
-b22_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b22_button.textContent == ""){
-        b22_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
-
-let b23_button=document.getElementById('b23');
-b23_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b23_button.textContent == ""){
-        b23_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
-
-let b31_button=document.getElementById('b31');
-b31_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b31_button.textContent == ""){
-        b31_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
-
-let b32_button=document.getElementById('b32');
-b32_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b32_button.textContent == ""){
-        b32_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
-
-let b33_button=document.getElementById('b33');
-b33_button.addEventListener("click", function (){
-    pmesage.textContent="";
-    if(b33_button.textContent == ""){
-        b33_button.textContent = turn;
-    }else{
-        pmesage.textContent="Already filled out!";
-    }
-    chek();
-    next();
-});
+    thisButton.addEventListener("click", function (){
+        if(!win) {
+            if(this.textContent == ""){
+                console.log(this.id);
+                this.textContent = turn;
+                chek();
+                next();
+                console.log(turn);
+            }else{
+                pmesage.textContent="Already filled out!";
+            }
+        }
+    });
+}
 
 function next ()
 {
@@ -133,29 +43,22 @@ function next ()
 }
 
 function chek (){
-    if(b11_button.textContent == b12_button.textContent && b12_button.textContent == b13_button.textContent && b11_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
-    if(b21_button.textContent == b22_button.textContent && b22_button.textContent == b23_button.textContent && b21_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
-    if(b31_button.textContent == b32_button.textContent && b32_button.textContent == b33_button.textContent && b31_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
-    if(b11_button.textContent == b21_button.textContent && b21_button.textContent == b31_button.textContent && b11_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
-    if(b12_button.textContent == b22_button.textContent && b22_button.textContent == b32_button.textContent && b12_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
-    if(b13_button.textContent == b23_button.textContent && b23_button.textContent == b33_button.textContent && b13_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
-    if(b11_button.textContent == b22_button.textContent && b33_button.textContent == b22_button.textContent && b11_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
-    if(b13_button.textContent == b22_button.textContent && b22_button.textContent == b31_button.textContent && b13_button.textContent != ""){
-        pmesage.textContent= turn + " Winn !!!";
-    }
+    linChek(0, 1, 2);
+    linChek(3, 4, 5);
+    linChek(6, 7, 8);
+    linChek(0, 4, 8);
+    linChek(2, 4, 6);
+    linChek(0, 3, 6);
+    linChek(1, 4, 7);
+    linChek(2, 5, 8);
 }
 
+function linChek(poz1, poz2, poz3){
+    if(myButtons[poz1].textContent == myButtons[poz2].textContent && myButtons[poz2].textContent == myButtons[poz3].textContent && myButtons[poz1].textContent != ""){
+        pmesage.textContent= turn + " Winn !!!";
+        win=true;
+        myButtons[poz1].style.background="#35f035";
+        myButtons[poz2].style.background="#35f035";
+        myButtons[poz3].style.background="#35f035";
+    }
+}
